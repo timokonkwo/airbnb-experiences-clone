1932de1bbc8c31c50dcf8b27f295c44b669b9cf1
timokonkwo <chibuiketimokonkwo@gmail.com>
2022-09-03 22:52:04 +0100

    added a normal react setup with vite

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Navbar(){
     return (
         <nav>
-            <img src="./images/airbnb-logo.png" className="logo"/>
+            <img src="../assets/images/airbnb-logo.png" className="logo"/>
         </nav>
     )
 }
