222894c653a83044c91d350a85e04badb757e75c
timokonkwo <chibuiketimokonkwo@gmail.com>
2022-09-03 20:27:48 +0100

    initial commit

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from "react"
+import Navbar from "./Navbar"
+import Home from "./Home"
+
+/*
+Challenge: Build the Navbar component.
+Check the Figma file for the design specifics.
+*/
+
+export default function App() {
+    return (
+        <div>
+            <Navbar/>
+            <Home />
+        </div>
+    )
+}
---
+++ Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Home(){
+    return (
+        <div className="container">
+            <div className="img__container">
+                <img src="images/photo-grid.png"/>
+            </div>
+            
+            <h1>Online Experiences</h1>
+            <p className="description"> 
+                Join unique interactive activities led by
+                one-of-a-kind hosts -all without leaving home
+            </p>
+        </div>
+    )
+}
