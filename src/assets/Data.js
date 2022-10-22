5675ac43b6e6fd069b66d4d59bf42e2c0326526a
Tim <chibuiketimokonkwo@gmail.com>
2022-10-22 09:19:10 +0100

    added images

--- src/assets/Data.js
+++ src/assets/Data.js
@@ -1,10 +1,10 @@
-const img = "https://via.placeholder.com/300";
+// const img = "https://via.placeholder.com/300";
 
 export default [
     {
         status: "SOLD OUT",
         title:"Life Lessons with Katie",
-        img, //"katie.png",
+        img:  "https://images.all-free-download.com/images/graphicwebp/x_shepherd_565662.webp",
         count: 6,
         price: 136
     },
@@ -12,7 +12,7 @@ export default [
     {
         status: "ONLINE",
         title: "Learn wedding photography",
-        img, // "wedding.png",
+        img: "https://images.all-free-download.com/images/graphicwebp/aires_ix_rocket_launch_pad_216982.webp",
         count: 30,
         price: 125
     },
@@ -20,7 +20,7 @@ export default [
     {
         status: "",
         title: "Group mountain biking",
-        img, // "bike.png",
+        img: "https://images.all-free-download.com/images/graphicwebp/x_515791.webp",
         count: 2,
         price: 50
     }
